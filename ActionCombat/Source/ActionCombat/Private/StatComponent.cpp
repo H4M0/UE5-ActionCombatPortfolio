5fec55531e30e01c8179c27d06da6222b7cbe929
H4M0 <khjzzang803@gmail.com>
2026-05-09 16:47:10 +0900

    Add stat component for health management

---
+++ ActionCombat/Source/ActionCombat/Private/StatComponent.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StatComponent.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UStatComponent::UStatComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+float UStatComponent::ApplyDamage(float DamageAmount)
+{
+	if (bIsDead)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s is already dead. Damage ignored."),
+			*GetOwner()->GetName());
+
+		return 0.0f;
+	}
+
+	if (DamageAmount <= 0.0f)
+	{
+		return 0.0f;
+	}
+
+	const float PreviousHealth = CurrentHealth;
+
+	CurrentHealth = FMath::Clamp(CurrentHealth - DamageAmount, 0.0f, MaxHealth);
+
+	const float ActualDamage = PreviousHealth - CurrentHealth;
+
+	UE_LOG(LogTemp, Warning, TEXT("%s took %.1f damage. HP: %.1f / %.1f"),
+		*GetOwner()->GetName(),
+		ActualDamage,
+		CurrentHealth,
+		MaxHealth);
+
+	if (CurrentHealth <= 0.0f)
+	{
+		bIsDead = true;
+
+		UE_LOG(LogTemp, Warning, TEXT("%s is dead."),
+			*GetOwner()->GetName());
+	}
+
+	return ActualDamage;
+}
+
+
+// Called when the game starts
+void UStatComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurrentHealth = MaxHealth;
+	bIsDead = false;
+
+	UE_LOG(LogTemp, Warning, TEXT("%s Stat Initialized. HP: %.1f / %.1f"),
+		*GetOwner()->GetName(),
+		CurrentHealth,
+		MaxHealth);
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UStatComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ ActionCombat/Source/ActionCombat/Public/StatComponent.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "StatComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ACTIONCOMBAT_API UStatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UStatComponent();
+
+	float ApplyDamage(float DamageAmount);
+
+	bool IsDead() const { return bIsDead; }
+	float GetCurrentHealth() const { return CurrentHealth; }
+	float GetMaxHealth() const { return MaxHealth; }
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats", meta = (AllowPrivateAccess = "true", ClampMin = "1.0"))
+	float MaxHealth = 100.0f;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stats", meta = (AllowPrivateAccess = "true"))
+	float CurrentHealth = 0.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stats", meta = (AllowPrivateAccess = "true"))
+	bool bIsDead = false;
+};
